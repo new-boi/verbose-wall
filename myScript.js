38304afca55b2b1af2476085c76cf3e6af5a0179
new-boi <136434275+new-boi@users.noreply.github.com>
2023-09-06 11:35:36 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,43 +1,33 @@
-const { google } = require('googleapis');
-const sheets = google.sheets('v4');
-
-const auth = new google.auth.GoogleAuth({
-  keyFile: 'pledge-wall-0b0598a13a50.json',
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
-
-async function iterateRows() {
-    const client = await auth.getClient();
-    const spreadsheetId = '1t-GrI2hdGaFUobDVyGCCl-zxfH5wP_2bErOdaDJXJuo';
-    const range = 'Sheet1'; // Update the sheet name and range as needed
-  
-    try {
-      const response = await sheets.spreadsheets.values.get({
-        auth: client,
-        spreadsheetId,
-        range,
-      });
-  
-      const rows = response.data.values;
-  
-      if (rows.length) {
-        rows.forEach((row, index) => {
-          // Assuming columns A, B, and C correspond to name, location, and sentence
-          const name = row[0];
-          const location = row[1];
-          const sentence = row[2];
-  
-          console.log(`Row ${index + 2}: Name: ${name}, Location: ${location}, Sentence: ${sentence}`);
+gapi.client.init({
+    apiKey: '0b0598a13a504cf8c531c0ef14fd426631fcd8c3',
+    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(() => {
+    function readDataFromSheet() {
+        gapi.client.sheets.spreadsheets.values.get({
+          spreadsheetId: '1t-GrI2hdGaFUobDVyGCCl-zxfH5wP_2bErOdaDJXJuo',
+          range: 'Sheet1', // Adjust the sheet and range as needed
+        }).then((response) => {
+          const values = response.result.values;
+      
+          if (values && values.length > 0) {
+            values.forEach((row, index) => {
+              const name = row[0];
+              const location = row[1];
+              const sentence = row[2];
+              console.log(`Row ${index + 2}: Name: ${name}, Location: ${location}, Sentence: ${sentence}`);
+            });
+          } else {
+            console.log('No data found in the specified range.');
+          }
+        }).catch((error) => {
+          console.error('Error reading data from Google Sheets:', error);
         });
-      } else {
-        console.log('No data found in the specified range.');
       }
-    } catch (err) {
-      console.error('Error reading data from Google Sheets:', err);
-    }
-  }
-  
-  iterateRows();
+  }).catch((error) => {
+    console.error('Error initializing Google API client:', error);
+  });
+
+
 
 
 var qr_btn = document.getElementById("qr_btn");
