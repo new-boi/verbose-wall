bd10860a097d144d8b15338d8d895fee3d3cf54e
new-boi <136434275+new-boi@users.noreply.github.com>
2023-09-06 12:26:12 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -27,7 +27,7 @@ function readDataFromSheet() {
   function initGoogleApiClient() {
     // Initialize the Google API client with your API key and discoveryDocs
     gapi.client.init({
-      apiKey:'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',
+      apiKey:'AIzaSyAM2PqI2i42qud73TZPP0XLfpFXk6DRueI',
       discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(() => {
       // Client is initialized and ready to make API calls
