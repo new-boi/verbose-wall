8af0ee524e58577c0782d032fec85871555b82b2
new-boi <136434275+new-boi@users.noreply.github.com>
2023-09-07 12:29:59 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,144 +1,67 @@
+const CLIENT_ID = '1030257145728-29cjk1r2f3i64gdjai4jd4ij8e03sem4.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAM2PqI2i42qud73TZPP0XLfpFXk6DRueI';
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
 
-
-function readDataFromSheet() {
-    gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId: '1t-GrI2hdGaFUobDVyGCCl-zxfH5wP_2bErOdaDJXJuo',
-      range: 'Sheet1', // Adjust the sheet and range as needed
-    }).then((response) => {
-      const values = response.result.values;
-  
-      if (values && values.length > 0) {
-        values.forEach((row, index) => {
-          const name = row[0];
-          const location = row[1];
-          const sentence = row[2];
-          console.log(`Row ${index + 2}: Name: ${name}, Location: ${location}, Sentence: ${sentence}`);
-        });
-      } else {
-        console.log('No data found in the specified range.');
-      }
-    }).catch((error) => {
-      console.error('Error reading data from Google Sheets:', error);
-    });
-  }
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
 
 
-  function writeToSheet(namee) {
-    const spreadsheetId = '1t-GrI2hdGaFUobDVyGCCl-zxfH5wP_2bErOdaDJXJuo';
-    const values = [namee, 'New Location', 'New Sentence'];
-
-    gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: 'Sheet1', // Get the entire sheet to find the last row
-    }).then((response) => {
-      const values = response.result.values || [];
-      const lastRow = values.length + 1; // Calculate the next row
-
-      gapi.client.sheets.spreadsheets.values.update({
-        spreadsheetId,
-        range: `Sheet1!A${lastRow}:C${lastRow}`, // Append to the last row
-        valueInputOption: 'RAW',
-        resource: { values },
-      }).then(() => {
-        console.log('Data added to the last row of Google Sheets.');
-      }).catch((error) => {
-        console.error('Error writing to Google Sheets:', error);
-      });
-    });
-
+function gapiLoaded() {
+  gapi.load('client', initializeGapiClient);
 }
 
 
-function writeTo() {
-  const spreadsheetId = 'your-spreadsheet-id';
-  const values = [['John Doe', 'New York', 'This is a sample sentence.']];
 
-  gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range: 'A1', // Update this to your desired range
-    valueInputOption: 'RAW',
-    resource: { values },
-  }).then(() => {
-    console.log('Data written to Google Sheets.');
-  }).catch((error) => {
-    console.error('Error writing to Google Sheets:', error);
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
   });
+  gapiInited = true;
+  maybeEnableButtons();
 }
 
 
-  function initGoogleApiClient() {
-    // Initialize the Google API client with your API key and discoveryDocs
-    gapi.client.init({
-      clientId: '1030257145728-jk2qipcuonehobecfa8taaggkqg04k2s.apps.googleusercontent.com',
-      scope: 'https://www.googleapis.com/auth/spreadsheets',
-      discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(() => {
-      writeTo();
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+  });
+  gisInited = true;
+  maybeEnableButtons();
+}
 
-        readDataFromSheet(); // Call the function that interacts with Google Sheets
-    }).catch((error) => {
-      console.error('Error initializing Google API client:', error);
-    });
+
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById('authorize_button').style.visibility = 'visible';
   }
+}
 
-
-  function initGoogleApiClientWrite(namee) {
-    // Initialize the Google API client with your API key and discoveryDocs
-    gapi.client.init({
-      apiKey: 'AIzaSyAM2PqI2i42qud73TZPP0XLfpFXk6DRueI',
-      discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(() => {
-
-        writeToSheet(namee);
-    }).catch((error) => {
-      console.error('Error initializing Google API client:', error);
+async function listMajors() {
+  let response;
+  try {
+    // Fetch first 10 files
+    response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: '1t-GrI2hdGaFUobDVyGCCl-zxfH5wP_2bErOdaDJXJuo',
+      range: 'Sheet1s',
     });
+  } catch (err) {
+    document.getElementById('content').innerText = err.message;
+    return;
   }
-
-
-
-  gapi.load('client', initGoogleApiClient);
-
-var qr_btn = document.getElementById("qr_btn");
-
-
-var pledge = document.getElementById("pledge").value;
-
-qr_btn.onclick=function(){
-    var namee = document.getElementById("namee").value;
-    // var card_container = document.getElementById("cards");
-
-    // var newDiv = document.createElement("div");
-    // newDiv.classList.add("card");
-
-    // var pName = document.createElement("p");
-    // var pLocation = document.createElement("p");
-    // var pCard_content = document.createElement("p");
-
-    // pName.textContent=namee;
-    // pLocation.textContent="Gombak";
-    // pCard_content.textContent="lorem";
-
-    // pName.classList.add("card_content","name");
-    // pLocation.classList.add("card_content","location");
-    // pCard_content.classList.add("card_content","contents");
-
-    // newDiv.appendChild(pName);
-    // newDiv.appendChild(pLocation);
-    // newDiv.appendChild(pCard_content);
-
-
-    // card_container.appendChild(newDiv);
-
-
-    // initGoogleApiClient(x)
-
-
-    // initGoogleApiClientWrite(namee);
-
-    writeToSheet(namee);
-
-
-    
+  const range = response.result;
+  if (!range || !range.values || range.values.length == 0) {
+    document.getElementById('content').innerText = 'No values found.';
+    return;
+  }
+  // Flatten to string to display
+  const output = range.values.reduce(
+      (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+      'Name, Major:\n');
+  document.getElementById('content').innerText = output;
 }
